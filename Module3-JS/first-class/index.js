f38d111ad88a50c607e5394dd69f518c17abb911
MaryamMunawar <itx.maryam172@gmail.com>
2023-08-14 23:32:00 +0500

    module 3 commit

---
+++ Module3-JS/first-class/index.js
@@ -0,0 +1,8 @@
+class Person{
+    constructor(name, email){
+        this.name = name
+        this.email = email 
+
+    }
+}
+// In class,all the variables which we call are called by using keyword this. 
